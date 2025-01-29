98060e4ee673f6cfd270f9dd042d048ae50937a2
Daniel <daniel.solisb@outlook.com>
2025-01-29 11:06:23 -0500

    cm_3

--- app/models.py
+++ app/models.py
@@ -111,7 +111,7 @@ class Measurement(db.Model):
     camera_id = db.Column(db.Integer, db.ForeignKey('camera.id'), nullable=False)
 
 #modelos hechos con deepseek
-#ja
+#jasfdgsfdhgsfdh
 # models.py
 
 # Modelo para Fase
